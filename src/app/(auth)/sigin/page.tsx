65c670741f10c2974be545ee9752ed7c7aed7fe6
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-03-30 10:37:47 -0300

    loading effect and routes with id

--- src/app/(auth)/sigin/page.tsx
+++ src/app/(auth)/sigin/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
+
 export const metadata = {
   title: 'Login',
 }
 
 export default function SignIn() {
-  return <h1>SignIn Content</h1>
+  return (
+    <div>
+      <h1>SignIn Content</h1>
+      <Link href="/dashboard">Go to Dashboard</Link>
+    </div>
+  )
 }
