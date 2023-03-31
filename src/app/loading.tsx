1266e28c30287332fdee48d1661185ab39d523b0
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-03-31 20:46:24 -0300

    testing loading skeleton

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,15 @@
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 export default function Loading() {
   return (
-    <div>
-      <h1>Loading</h1>
-    </div>
+    <SkeletonTheme
+      width={500}
+      height={100}
+      baseColor="#202020"
+      highlightColor="#444"
+    >
+      <Skeleton count={3} />
+    </SkeletonTheme>
   )
 }
